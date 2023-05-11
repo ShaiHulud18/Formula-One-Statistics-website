e9203b261ee9397384f14593dbecc8fb6df73826
ShaiHulud18 <98670721+ShaiHulud18@users.noreply.github.com>
2023-05-11 15:23:37 -0700

    Update drivers.php

--- drivers.php
+++ drivers.php
@@ -122,6 +122,7 @@
         		die(print_r(sqlsrv_errors(), true));
     			}
 
+			
  
     			while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
         			echo '<option value="' . $row['driverName'] . '">' . $row['driverName'] . '</option>';
